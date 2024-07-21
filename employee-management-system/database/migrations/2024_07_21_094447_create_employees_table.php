3b5269aaccac184d961d3a758845d757c498d254
IT21158186 <it21158186@my.sliit.lk>
2024-07-21 15:22:14 +0530

    db- phone-nullable set

--- employee-management-system/database/migrations/2024_07_21_094447_create_employees_table.php
+++ employee-management-system/database/migrations/2024_07_21_094447_create_employees_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('emp_name');
             $table->date('dob');
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
